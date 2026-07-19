78121fbc102c8766333e59d070d95dd3bbbda63f
onlychirag <chiragkakwani.001@gmail.com>
2026-07-19 12:19:14 +0530

    fix: SVG viewBox syntax error in Docs.jsx

--- src/Docs.jsx
+++ src/Docs.jsx
@@ -114,7 +114,7 @@ function Globe({ size, className }) {
     <svg 
       xmlns="http://www.w3.org/2000/svg" 
       width={size} height={size} 
-      viewBox="0 24" 
+      viewBox="0 0 24 24" 
       fill="none" 
       stroke="currentColor" 
       strokeWidth="2" 
